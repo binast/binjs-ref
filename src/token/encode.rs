7ef133bd09bf79d1436069a3c32f7ac133577339
David Teller <D.O.Teller@gmail.com>
2017-06-12 21:07:11 +0200

    Encoding: Improving error messages

--- src/token/encode.rs
+++ src/token/encode.rs
@@ -27,6 +27,12 @@ impl<E> From<E> for Error<E> {
     }
 }
 
+impl<E> Error<E> {
+    fn missing_field(field: &str, node: &NodeName) -> Self {
+        Error::MissingField(format!("Field \"{}\" while handling {:?}", field, node))
+    }
+}
+
 
 pub struct Encoder<'a, B, Tree, E> where B: TokenWriter<Tree=Tree, Error=E>, E: Debug {
     grammar: &'a Syntax,
@@ -48,6 +54,9 @@ impl<'a, B, Tree, E> Encoder<'a, B, Tree, E> where B: TokenWriter<Tree=Tree, Err
     /// Encode a JSON into a SerializeTree based on a grammar.
     /// This step doesn't perform any interesting check on the JSON.
     pub fn encode(&self, value: &Value, kind: &Type) -> Result<Tree, Error<E>> {
+        self.encode_aux(value, kind, self.grammar.get_start().name())
+    }
+    fn encode_aux(&self, value: &Value, kind: &Type, node: &NodeName) -> Result<Tree, Error<E>> {
         debug!("encode: value {:?} with kind {:?}", value, kind);
         use ast::grammar::Type::*;
         match *kind {
@@ -59,7 +68,7 @@ impl<'a, B, Tree, E> Encoder<'a, B, Tree, E> where B: TokenWriter<Tree=Tree, Err
                     })?;
                 let mut encoded = Vec::with_capacity(list.len());
                 for item in list {
-                    let item = self.encode(item, kind)?;
+                    let item = self.encode_aux(item, kind, node)?;
                     encoded.push(item);
                 }
                 let result = self.builder.borrow_mut().list(encoded)
@@ -72,7 +81,7 @@ impl<'a, B, Tree, E> Encoder<'a, B, Tree, E> where B: TokenWriter<Tree=Tree, Err
                         expected: "Object".to_string(),
                         got: type_of(&value)
                     })?;
-                let mut contents = self.encode_structure(object, structure.fields())?;
+                let mut contents = self.encode_structure(object, structure.fields(), node)?;
                 let contents : Vec<_> = contents
                     .drain(..)
                     .map(|(_, tree)| tree)
@@ -88,7 +97,7 @@ impl<'a, B, Tree, E> Encoder<'a, B, Tree, E> where B: TokenWriter<Tree=Tree, Err
                     .ok_or_else(|| Error::NoSuchEnum(name.to_string().clone()))?;
                 let string = value.as_str()
                     .ok_or_else(|| Error::Mismatch {
-                        expected: "String".to_string(),
+                        expected: format!("String (one of {:?}), while treating {:?}", enum_, node),
                         got: type_of(&value)
                     })?;
                 for candidate in enum_.strings() {
@@ -118,11 +127,11 @@ impl<'a, B, Tree, E> Encoder<'a, B, Tree, E> where B: TokenWriter<Tree=Tree, Err
            } => {
                let object = value.as_object()
                    .ok_or_else(|| Error::Mismatch {
-                       expected: "Object (implementing interface)".to_string(),
+                       expected: format!("Object (implementing {:?}) while treating {:?}", interfaces, node),
                        got: type_of(&value)
                    })?;
                let type_field = object.get("type")
-                   .ok_or_else(|| Error::MissingField("type".to_string()))?;
+                   .ok_or_else(|| Error::missing_field("type", node))?;
                let kind_name = type_field
                    .as_str()
                    .ok_or_else(|| Error::Mismatch {
@@ -151,7 +160,7 @@ impl<'a, B, Tree, E> Encoder<'a, B, Tree, E> where B: TokenWriter<Tree=Tree, Err
                         return Err(Error::NoSuchRefinement(kind.to_string().clone()))
                    }
                    let fields = interface.contents().fields();
-                   let contents = self.encode_structure(object, fields)?;
+                   let contents = self.encode_structure(object, fields, interface.name())?;
                    // Write the contents with the tag of the refined interface.
                    let labelled = self.builder
                        .borrow_mut()
@@ -199,14 +208,14 @@ impl<'a, B, Tree, E> Encoder<'a, B, Tree, E> where B: TokenWriter<Tree=Tree, Err
         }
     }
 
-    fn encode_structure<'b>(&self, object: &'b Object, fields: &'b [Field]) -> Result<Vec<(&'b Field, B::Tree)>, Error<E>> {
+    fn encode_structure<'b>(&self, object: &'b Object, fields: &'b [Field], node: &NodeName) -> Result<Vec<(&'b Field, B::Tree)>, Error<E>> {
         let mut result = Vec::with_capacity(fields.len());
         for field in fields {
             if let Some(source) = object.get(field.name().to_string()) {
-                let encoded = self.encode(source, field.type_())?;
+                let encoded = self.encode_aux(source, field.type_(), node)?;
                 result.push((field, encoded))
             } else {
-                return Err(Error::MissingField(field.name().to_string().clone()))
+                return Err(Error::missing_field(field.name().to_string(), node));
             }
         }
         Ok(result)
