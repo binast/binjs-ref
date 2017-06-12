d98fba546a29abd6f2979c673cb4e35af19565ff
David Teller <D.O.Teller@gmail.com>
2017-06-12 23:57:21 +0200

    Babel: now using temporary files for communication between processes.
    We quickly hit the size limit of passing all data through `-e`.

--- src/source/babel.rs
+++ src/source/babel.rs
@@ -2,19 +2,23 @@ use serde_json;
 use serde_json::Value as JSON;
 
 use std;
-use std::io::Cursor;
+use std::io::{ Cursor, Read, Write };
 use std::path::*;
 use std::process::*;
 
 use source::parser::SourceParser;
+use util::get_temporary_file;
 
 #[derive(Debug)]
 pub enum Error {
     CouldNotLaunch(std::io::Error),
+    CouldNotReadFile(std::io::Error),
     ExecutionError(std::io::Error),
+    CouldNotCreateFile(std::io::Error),
     ReturnedError(ExitStatus),
     JsonError(serde_json::Error),
     InvalidPath(PathBuf),
+    InvalidUTF8(std::string::FromUtf8Error),
     InvalidAST,
 }
 
@@ -34,11 +38,32 @@ impl Babel {
         }
     }
 
-    fn parse_script_output(&self, script: &str) -> Result<JSON, Error> {
-        info!(target: "Babel", "Launching script {}", script);
+    fn parse_script_output(&self, script: &str) -> Result<String, Error> {
+        debug!(target: "Babel", "Writing script {}", script);
+        let (path, mut channel) = get_temporary_file("js")
+            .map_err(Error::CouldNotCreateFile)?;
+
+        let script = format!(r##"
+        var result  = (() => {{
+            {}
+        }})();
+        var fs     = require('fs');
+        var mktemp = require('mktemp');
+        var out    = mktemp.createFileSync("/tmp/binjs-XXXXXXXX.out");
+        fs.writeFileSync(out, result);
+        console.log(out);
+        "##,
+        script);
+
+        println!("Executing script {} to {:?}", script, path);
+        println!("Root: {}", env!("CARGO_MANIFEST_DIR"));
+        channel.write(script.as_bytes())
+            .map_err(Error::CouldNotCreateFile)?;
+
+        debug!(target: "Babel", "Launching script {}", script);
         let output = Command::new(&*self.bin_path)
-            .arg("-e")
-            .arg(script)
+            .arg(path)
+            .env("NODE_PATH", "node_modules")
             .stdin(Stdio::null())
             .stdout(Stdio::piped())
             .stderr(Stdio::piped())
@@ -51,22 +76,48 @@ impl Babel {
             return Err(Error::ReturnedError(output.status));
         }
 
-        debug!(target: "Babel", "JSON {:?}", String::from_utf8(output.stdout.clone()).unwrap());
+        let return_path = String::from_utf8(output.stdout.clone())
+            .map_err(Error::InvalidUTF8)?;
+        let return_path = return_path
+            .trim_right();
+
+        println!("Return path: '{}'", return_path);
+        let mut file = std::fs::File::open(return_path)
+            .map_err(Error::CouldNotReadFile)?;
+
+        println!("Opened file");
+        let mut result = String::new();
+        file.read_to_string(&mut result)
+            .map_err(Error::CouldNotReadFile)?;
+        Ok(result)
+    }
+
+    fn parse_script_json_output(&self, script: &str) -> Result<JSON, Error> {
+        let stdout = self.parse_script_output(script)?;
 
         // Now attempt to parse JSON
-        let data = Cursor::new(output.stdout);
-        let result: JSON = serde_json::from_reader(data)
-            .map_err(Error::JsonError)?;
+        let data = Cursor::new(stdout);
+        serde_json::from_reader(data)
+            .map_err(Error::JsonError)
+    }
 
-        // Extract meaningful subset
-        if let JSON::Object(mut obj) = result {
-            let subset = obj.remove("program")
-                .ok_or(Error::InvalidAST)?;
-            return Ok(subset);
-        }
+    pub fn to_source(&self, ast: &JSON) -> Result<String, Error> {
+        // Escape `"`.
+        let data = serde_json::to_string(ast)
+            .map_err(Error::JsonError)?
+            .replace("\\", "\\\\")
+            .replace("\"", "\\\"");
 
-        // Result should be a valid AST.
-        Err(Error::InvalidAST)
+        // A script to parse a string, write it to stdout as JSON.
+        let script = format!(
+            r##"
+            var babel   = require('babel-core');
+            var ast     = JSON.parse("{}");
+            var parsed  = babel.transformFromAst(ast);
+            return parsed.code;
+            "##,
+            data);
+        self.parse_script_output(&script)
     }
 }
 
@@ -82,11 +133,10 @@ impl SourceParser for Babel {
             var babylon = require('babylon');
 
             var parsed  = babylon.parse("{}");
-            var json    = JSON.stringify(parsed);
-            process.stdout.write(json)
+            return JSON.stringify(parsed.program);
             "##,
             data);
-        self.parse_script_output(&script)
+        self.parse_script_json_output(&script)
     }
 
     /// Parse a text source file, using Babel.
@@ -102,11 +152,10 @@ impl SourceParser for Babel {
 
             var source  = fs.readFileSync('{}', {{encoding: "utf-8"}});
             var parsed  = babylon.parse(source);
-            var json    = JSON.stringify(parsed);
-            process.stdout.write(json)
+            return JSON.stringify(parsed.program);
             "##,
             path);
-        self.parse_script_output(&script)
+        self.parse_script_json_output(&script)
     }
 }
 
--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,6 +1,10 @@
+use rand;
 use serde_json::{ Value as JSON, Number };
 
+use std;
+use std::fs::File;
 use std::io::{ Read, Result, Seek, SeekFrom };
+use std::path::*;
 
 pub struct PositionRead<R> where R: Read {
     reader: R,
@@ -79,4 +83,31 @@ pub fn f64_of(number: &Number) -> f64 {
     } else {
         number.as_f64().unwrap()
     }
+}
+
+pub fn get_temporary_file(extension: &str) -> std::result::Result<(PathBuf, File), std::io::Error> {
+    use rand::Rng;
+    let directory = std::env::temp_dir();
+    let mut rng = rand::os::OsRng::new()
+        .unwrap();
+    let mut ascii = rng.gen_ascii_chars();
+    let mut buf = Vec::with_capacity(8);
+    let mut error = None;
+    const ATTEMPTS : usize = 1024;
+    for _ in 0..ATTEMPTS { // Limit number of attempts
+        // FIXME: There must be a nicer way to do this.
+        buf.clear();
+        for _ in 0..8 {
+            buf.push(ascii.next().unwrap());
+        }
+        let name : String = buf.iter().collect();
+        let path = directory.as_path()
+            .join(format!("binjs-{}.{}", name, extension));
+        let result = File::create(&path);
+        match result {
+            Ok(file) => return Ok((path, file)),
+            Err(err) => error = Some(err)
+        }
+    }
+    Err(error.unwrap())
 }
