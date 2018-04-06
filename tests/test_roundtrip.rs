04dc6fda9237e5b8cde7d3e4274117d9ec668a53
David Teller <D.O.Teller@gmail.com>
2018-04-06 09:58:52 +0200

    Resolves #92 Deactivate Deflate compression
    We encounter mysterious problems with Deflate compression, which may be related to https://github.com/alexcrichton/flate2-rs/issues/151 . Deactivating Deflate compression for the time being.

--- tests/test_roundtrip.rs
+++ tests/test_roundtrip.rs
@@ -30,7 +30,7 @@ fn test_roundtrip() {
     let all_options = {
         use self::Compression::*;
         let mut vec = vec![];
-        let compressions = [Identity, Gzip, Deflate, Brotli, /*Lzw doesn't work yet*/];
+        let compressions = [Identity, Gzip, /*Deflate seems broken upstream,*/ Brotli, /*Lzw doesn't work yet*/];
         for grammar_table in &compressions {
             for strings_table in &compressions {
                 for tree in &compressions {
@@ -45,86 +45,88 @@ fn test_roundtrip() {
         vec
     };
 
-    let path = format!("{}/tests/data/spidermonkey/ecma_2/**/*.js", env!("CARGO_MANIFEST_DIR"));
-    debug!(target: "test_roundtrip", "Starting test_roundtrip from {}", path);
+    for path_suffix in &["tests/data/facebook/single/**/*.js", "tests/data/spidermonkey/ecma_2/**/*.js"] {
+        let path = format!("{}/{}", env!("CARGO_MANIFEST_DIR"), path_suffix);
+        debug!(target: "test_roundtrip", "Starting test_roundtrip from {}", path);
 
-    for entry in glob::glob(&path)
-        .expect("Invalid glob pattern")
-    {
-        let entry = entry.expect("Invalid entry");
-
-        // Parse and preprocess file.
-
-        println!("Parsing {:?}.", entry);
-        let mut ast    = parser.parse_file(entry.clone())
-            .expect("Could not parse source");
-        debug!(target: "test_roundtrip", "Source: {}", ast.pretty(2));
-        debug!(target: "test_roundtrip", "Annotating {:?}.", entry);
-
-        let mut ast = binjs::specialized::es6::ast::Script::import(&ast)
-            .expect("Could not import AST");
-        binjs::specialized::es6::scopes::AnnotationVisitor::new()
-            .annotate_script(&mut ast);
-
+        for entry in glob::glob(&path)
+            .expect("Invalid glob pattern")
         {
-            progress();
-            debug!(target: "test_roundtrip", "Starting simple round trip for {:?}", entry);
+            let entry = entry.expect("Invalid entry");
 
-            // Roundtrip `simple`
-            debug!(target: "test_roundtrip", "Encoding");
-            let mut writer = binjs::io::simple::TreeTokenWriter::new();
-            let mut serializer = binjs::specialized::es6::io::Serializer::new(writer);
-            serializer.serialize(&ast)
-                .expect("Could not encode AST");
-            let (data, _) = serializer.done()
-                .expect("Could not finalize AST encoding");
+            // Parse and preprocess file.
 
-            progress();
-            debug!(target: "test_roundtrip", "Decoding.");
-            let source = Cursor::new(data);
-            let reader = binjs::io::simple::TreeTokenReader::new(source);
-            let mut deserializer = binjs::specialized::es6::io::Deserializer::new(reader);
+            println!("Parsing {:?}.", entry);
+            let mut ast    = parser.parse_file(entry.clone())
+                .expect("Could not parse source");
+            debug!(target: "test_roundtrip", "Source: {}", ast.pretty(2));
+            debug!(target: "test_roundtrip", "Annotating {:?}.", entry);
 
-            let decoded = deserializer.deserialize()
-                .expect("Could not decode");
-            progress();
+            let mut ast = binjs::specialized::es6::ast::Script::import(&ast)
+                .expect("Could not import AST");
+            binjs::specialized::es6::scopes::AnnotationVisitor::new()
+                .annotate_script(&mut ast);
 
-            debug!(target: "test_roundtrip", "Checking.");
-            assert_eq!(ast, decoded);
+            {
+                progress();
+                debug!(target: "test_roundtrip", "Starting simple round trip for {:?}", entry);
 
-            debug!(target: "test_roundtrip", "Completed simple round trip for {:?}", entry);
-        }
+                // Roundtrip `simple`
+                debug!(target: "test_roundtrip", "Encoding");
+                let mut writer = binjs::io::simple::TreeTokenWriter::new();
+                let mut serializer = binjs::specialized::es6::io::Serializer::new(writer);
+                serializer.serialize(&ast)
+                    .expect("Could not encode AST");
+                let (data, _) = serializer.done()
+                    .expect("Could not finalize AST encoding");
 
-        // Roundtrip `multipart`
+                progress();
+                debug!(target: "test_roundtrip", "Decoding.");
+                let source = Cursor::new(data);
+                let reader = binjs::io::simple::TreeTokenReader::new(source);
+                let mut deserializer = binjs::specialized::es6::io::Deserializer::new(reader);
 
-        for options in &all_options {
-            progress();
-            debug!(target: "test_roundtrip", "Starting multipart round trip for {:?} with options {:?}", entry, options);
-            debug!(target: "test_roundtrip", "Encoding.");
-            let writer  = binjs::io::multipart::TreeTokenWriter::new(options.clone());
-            let mut serializer = binjs::specialized::es6::io::Serializer::new(writer);
-            serializer.serialize(&ast)
-                .expect("Could not encode AST");
-            let (data, _) = serializer.done()
-                .expect("Could not finalize AST encoding");
+                let decoded = deserializer.deserialize()
+                    .expect("Could not decode");
+                progress();
 
-            progress();
+                debug!(target: "test_roundtrip", "Checking.");
+                assert_eq!(ast, decoded);
 
-            debug!(target: "test_roundtrip", "Decoding.");
-            let source = Cursor::new(data);
-            let reader = binjs::io::multipart::TreeTokenReader::new(source)
-                .expect("Could not decode AST container");
-            let mut deserializer = binjs::specialized::es6::io::Deserializer::new(reader);
+                debug!(target: "test_roundtrip", "Completed simple round trip for {:?}", entry);
+            }
 
-            let decoded = deserializer.deserialize()
-                .expect("Could not decode");
-            progress();
+            // Roundtrip `multipart`
 
-            debug!(target: "test_roundtrip", "Checking.");
-            assert_eq!(ast, decoded);
+            for options in &all_options {
+                progress();
+                debug!(target: "test_roundtrip", "Starting multipart round trip for {:?} with options {:?}", entry, options);
+                debug!(target: "test_roundtrip", "Encoding.");
+                let writer  = binjs::io::multipart::TreeTokenWriter::new(options.clone());
+                let mut serializer = binjs::specialized::es6::io::Serializer::new(writer);
+                serializer.serialize(&ast)
+                    .expect("Could not encode AST");
+                let (data, _) = serializer.done()
+                    .expect("Could not finalize AST encoding");
 
-            debug!(target: "test_roundtrip", "Completed multipart round trip for {:?} with options {:?}", entry, options);
-            progress();
+                progress();
+
+                debug!(target: "test_roundtrip", "Decoding.");
+                let source = Cursor::new(data);
+                let reader = binjs::io::multipart::TreeTokenReader::new(source)
+                    .expect("Could not decode AST container");
+                let mut deserializer = binjs::specialized::es6::io::Deserializer::new(reader);
+
+                let decoded = deserializer.deserialize()
+                    .expect("Could not decode");
+                progress();
+
+                debug!(target: "test_roundtrip", "Checking.");
+                assert_eq!(ast, decoded);
+
+                debug!(target: "test_roundtrip", "Completed multipart round trip for {:?} with options {:?}", entry, options);
+                progress();
+            }
         }
     }
 }
