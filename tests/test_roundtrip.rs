17c285ceb70104a7bf8420627c42d4e060610f19
David Teller <D.O.Teller@gmail.com>
2018-04-14 19:44:57 +0200

    Resolves 83 - Using rewriter to make functions skippable

--- tests/test_roundtrip.rs
+++ tests/test_roundtrip.rs
@@ -11,8 +11,11 @@ use binjs::io::bytes::compress::*;
 use binjs::io::multipart::*;
 use binjs::io::*;
 use binjs::source::*;
+use binjs::specialized::es6::ast::Walker;
 
 use std::io::*;
+
+const PATHS : [&'static str; 1] = [/* "tests/data/facebook/single/**/*.js", */ "tests/data/spidermonkey/ecma_2/**/*.js"];
 
 fn progress() {
     // Make sure that we see progress in the logs, without spamming these logs.
@@ -45,7 +48,72 @@ fn test_roundtrip() {
         vec
     };
 
-    for path_suffix in &["tests/data/facebook/single/**/*.js", "tests/data/spidermonkey/ecma_2/**/*.js"] {
+    print!("\nTesting roundtrip with laziness");
+
+    for path_suffix in &PATHS {
+        let path = format!("{}/{}", env!("CARGO_MANIFEST_DIR"), path_suffix);
+        debug!(target: "test_roundtrip", "Starting laziness test_roundtrip from {}", path);
+
+        for entry in glob::glob(&path)
+            .expect("Invalid glob pattern")
+        {
+            let entry = entry.expect("Invalid entry");
+
+            // Parse and preprocess file.
+
+            print!("\nPreparing laziness test for {:?}.", entry);
+            let mut ast    = parser.parse_file(entry.clone())
+                .expect("Could not parse source");
+
+            let mut ast = binjs::specialized::es6::ast::Script::import(&ast)
+                .expect("Could not import AST");
+            binjs::specialized::es6::scopes::AnnotationVisitor::new()
+                .annotate_script(&mut ast);
+
+            // Keep immutable.
+            let reference_ast = ast;
+            for level in &[0, 1, 2, 3, 4, 5] {
+                let mut ast = reference_ast.clone();
+                print!("\nLaziness level: {}", level);
+                debug!(target: "test_roundtrip", "Laziness level {}", level);
+
+                let mut path = binjs::specialized::es6::ast::Path::new();
+                let mut visitor = binjs::specialized::es6::skip::LazifierVisitor::new(*level);
+                ast.walk(&mut path, &mut visitor)
+                    .expect("Could not introduce laziness");
+                progress();
+
+                let options = WriteOptions {
+                    grammar_table: Compression::Identity,
+                    strings_table: Compression::Identity,
+                    tree: Compression::Identity,
+                };
+                debug!(target: "test_roundtrip", "Encoding.");
+                let writer  = binjs::io::multipart::TreeTokenWriter::new(options.clone());
+                let mut serializer = binjs::specialized::es6::io::Serializer::new(writer);
+                serializer.serialize(&ast)
+                    .expect("Could not encode AST");
+                let (data, _) = serializer.done()
+                    .expect("Could not finalize AST encoding");
+                progress();
+
+                debug!(target: "test_roundtrip", "Decoding.");
+                let source = Cursor::new(data);
+                let reader = binjs::io::multipart::TreeTokenReader::new(source)
+                    .expect("Could not decode AST container");
+                let mut deserializer = binjs::specialized::es6::io::Deserializer::new(reader);
+
+                let decoded = deserializer.deserialize()
+                    .expect("Could not decode");
+                progress();
+
+                debug!(target: "test_roundtrip", "Checking.");
+                assert_eq!(ast, decoded);
+            }
+        }
+    }
+
+    for path_suffix in &PATHS {
         let path = format!("{}/{}", env!("CARGO_MANIFEST_DIR"), path_suffix);
         debug!(target: "test_roundtrip", "Starting test_roundtrip from {}", path);
 
@@ -56,7 +124,7 @@ fn test_roundtrip() {
 
             // Parse and preprocess file.
 
-            println!("Parsing {:?}.", entry);
+            print!("\nParsing {:?}.", entry);
             let mut ast    = parser.parse_file(entry.clone())
                 .expect("Could not parse source");
             debug!(target: "test_roundtrip", "Source: {}", ast.pretty(2));
