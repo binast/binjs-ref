79b6bf318ff59506cb77fb31d0fb75574cb524f6
David Teller <D.O.Teller@gmail.com>
2018-08-02 09:45:14 +0200

    [WIP] Some doc

--- crates/binjs_io/src/multiarith.rs
+++ crates/binjs_io/src/multiarith.rs
@@ -24,12 +24,97 @@
 //! Similarly, we'll encode `left` and `right` in context C + `BinaryOperation`.
 //!
 //!
+//! # Format
+//!
+//! Magic Header:
+//!  TBD
+//!
+//! Grammar Header:
+//! - reference to an existing grammar + probability set; or
+//! - start from nothing
+//!
+//! Parameters Header:
+//!  packet_structure_len (number of bits to be read in a row)
+//!  TBD
+//!
+//! Tree:
+//!  Packet:
+//!    `packet_structure_len` bits, to be arithmetically decoded as a variable number
+//!         of u32, based on known probability distribution.
+//!         The mapping from u32 to the AST is heavily dependent on the context.
+//!         The grammar determines the nature of the value:
+//!         - tag;
+//!         - list length;
+//!         - string;
+//!         - string enum;
+//!         - number;
+//!         - boolean;
+//!         - variable declaration;
+//!         - variable reference;
+//!         - nullable variants.
+//!
+//!     arbitrary length definition
+//!
+//! # Decoding and interpreting packet structure
+//!
+//! Decoding a packet structure depends on:
+//! - context;
+//! - probabilities, which are given by context.
+//!
+//! In many cases, we won't be able to **extract** the next number of a packet structure
+//! until we have fully **interpreted** the previous numbers from the same packet structure,
+//! which may in turn need us to have read and interpreted a **definition** that appears
+//! after the packet structure.
+//!
+//! Conceptually, we have two streams:
+//! - one stream of u32 (*tree stream*), which reads from `packet_structure_len` bits;
+//! - one stream of *definitions*, which reads from the definition just after the `packet_structure_len` bits.
+//!
+//! Reading from those streams is interleaved, which may make things a bit complicated.
+//!
 //! # Extensibility
 //!
 //! While we can have a built-in initial dictionary, the list of symbols cannot be
 //! fully hardcoded, as there is no guarantee that the decoder won't be designed
 //! for a more recent version of the language, which may have different nodes/enums,
 //! etc.
+//!
+//! # Decoding a number
+//!
+//! Depending on the nature of the value:
+//! - boolean
+//!   TreeStream: 0 => false (no definition needed)
+//!   TreeStream: 1 => true (no definition needed)
+//!
+//! - tag in context C for which we have no definition
+//!    DefStream: tag id (varnum)
+//!     - if `tag id` is unknown
+//!         DefStream: tag grammar definition (see below)
+//!     - else
+//!         DefStream: `probabilities id` (varnum?)
+//!         - if `tag id::probabilities id` is unknown
+//!           DefStream: tag probabilities definition(number of items matching `tag id`) (see below)
+//!     now that tag has a definition, proceed
+//!
+//! - tag in context C for which we have a definition
+//!    TreeStream: n => tag[n]
+//!
+//! - tag grammar definition
+//!    DefStream: number of items (varnum)
+//!    - for i in 0..number of items
+//!       DefStream: name length (varnum)
+//!       DefStream: tag name (WTF-8)
+//!         may contain a special "null" tag name "", to represent null values
+//!         may contain a special "escape" tag name "?", to force loading a new list of items
+//!    DefStream: `probabilities id` (varnum?)
+//!    DefStream: tag probabilities definition(number of items) (see below)
+//!
+//! // FIXME: We probably want to predefine the tags in a header.
+//! // FIXME: Pass as argument a prepopulated probability distribution, generated from the library. By default, everything is set to 1.
+//!
+//! - tag probabilities definition(number of items)
+//!   - for i in 0..number of items
+//!     DefStream: relative number of instances of name `tag name[i]`, renormalized so that max == 255 (8 bits)
 //!
 //!
 //!
@@ -42,6 +127,18 @@
 //!
 //! FIXME: Some symbols will need to be defined more than once. Need to estimate
 //! how often this appears.
+//!
+//! FIXME: Wait, no, that doesn't work. If we want this to work, we first need
+//! to deliver the probability table for everything. That's not what we want.
+//!
+//! Option: we may initialize the dictionary as follows:
+//! - initial probability of a new symbol is 1.
+//! - whenever we create a symbol, we followup with its actual probability
+//! - the probability of new symbols decreases each time we add a symbol
+//!
+//!
+//! ----- Initially, start with everything equi-likely. We'll add a predefined
+//! and/or custom dictionary later.
 
 use io::TokenWriter;
 use ::{ DictionaryPlacement, CompressionTarget, TokenWriterError };
@@ -260,7 +357,7 @@ impl Compressor {
                 unimplemented!("FIXME: We now have the probability of `(subtree, parent)`. Use it to refine the current segment");
                 let appearance = appearance.borrow_mut();
                 if let Appearance::First = *appearance {
-                    unimplemented!("FIXME: Add definition of the current label");
+                    unimplemented!("FIXME: Append definition of the current label");
                 }
             }
             _ => {
