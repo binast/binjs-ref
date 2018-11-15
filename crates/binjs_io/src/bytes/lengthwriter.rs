f2c296b3c31c2c3d94d27a591074126475ed0899
David Teller <D.O.Teller@gmail.com>
2018-11-15 10:46:44 +0100

    New tool: lengthwriter

---
+++ crates/binjs_io/src/bytes/lengthwriter.rs
@@ -0,0 +1,35 @@
+use std::io::{ Error, Write };
+
+/// An implementation of `Write` that discards its data but remembers
+/// how many bytes were written.
+#[derive(Default)]
+pub struct LengthWriter {
+    bytes_written: usize,
+}
+impl LengthWriter {
+    /// Create an empty LengthWriter.
+    pub fn new() -> Self {
+        LengthWriter {
+            bytes_written: 0
+        }
+    }
+
+    /// Return the number of bytes written since creation.
+    pub fn len(&self) -> usize {
+        self.bytes_written
+    }
+}
+
+impl Write for LengthWriter {
+    /// Do nothing.
+    fn flush(&mut self) -> Result<(), Error> {
+        Ok(())
+    }
+
+    /// Pretend to successfully write bytes, only record the number
+    /// of bytes meant to be written.
+    fn write(&mut self, buf: &[u8]) -> Result<usize, Error> {
+        self.bytes_written += buf.len();
+        Ok(buf.len())
+    }
+}
