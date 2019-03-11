364a155af3f298edbe66668edaf1cb79b05efc44
Ingvar Stepanyan <me@rreverser.com>
2019-03-11 14:00:33 +0100

    Move BinaryAST -> Shift JSON conversion to JS side
    This is the next step of #342.
    
    Unlike the previous PR (#346), this not only tries to do 1:1 translation from Rust code to JavaScript, but also somewhat simplifies it where possible:
    
     - Ignores extra properties rather than trying to delete them, since Shift codegen will ignore them as well.
     - Merges all function branches into a catch-all regex.
    
    Also one annoying complication is that JS conversion can't check the interface name it's converting into, so I had to hardcode some parents where `Block` and `VariableDeclaration` are not used as statements. These places are commented and hopefully won't be a big issue to maintain.

--- src/source/codegen.js
+++ src/source/codegen.js
@@ -9,6 +9,10 @@ const codegen = require('shift-codegen').default;
 const startJSONStream = require('./start-json-stream');
 
 startJSONStream({
+    // `fromJSON` is a `JSON.parse` reviver callback that converts BinaryAST
+    // JSON to Shift AST compatible objects on the fly.
+    fromJSON: require('./to-shift'),
+
     // Mirrors the Rust side of generic value transformations.
     //
     // This one takes an AST from the Rust side as an input and applies
--- src/source/to-shift.js
+++ src/source/to-shift.js
@@ -1,31 +1,130 @@
 'use strict';
 
-function removeFunctionContents(obj) {
-    obj.type = obj.type.replace(/^(?:Eager|Lazy)/, '');
+/**
+ * This helper accepts arbitrary BinaryAST node and checks if it represents a function.
+ * If it is, it will be modified to conform to corresponding Shift AST interface.
+ * If it's not, the node will remain intact and function will bail out.
+ * @param {object} obj - a BinaryAST node as a plain JavaScript object.
+ */
+function tryRemoveFunctionContents(obj) {
+    let m = obj.type.match(/^(?:Eager|Lazy)(.*)$/);
+    let hasExpressionBody = false;
 
-    delete obj.isAsync;
-    delete obj.scope;
-    delete obj.contents_skip;
-    delete obj.length;
+    switch (m && m[1]) {
+        case 'ArrowExpressionWithExpression':
+            hasExpressionBody = true;
+            // fallthrough
+        case 'ArrowExpressionWithFunctionBody':
+            obj.type = 'ArrowExpression';
+            break;
+        case 'FunctionExpression':
+        case 'FunctionDeclaration':
+        case 'Method':
+        case 'Getter':
+        case 'Setter':
+            obj.type = m[1];
+            break;
+        default:
+            // not a function node, bail out
+            return;
+    }
 
     let { params, body } = obj.contents;
-    delete obj.contents;
 
     obj.params = params;
 
-    if (obj.type === 'ArrowExpressionWithExpression') {
-        obj.type = 'ArrowExpression';
-        obj.body = body;
-    } else {
-        if (obj.type === 'ArrowExpressionWithFunctionBody') {
-            obj.type = 'ArrowExpression';
-        }
-        obj.body = {
+    if (!hasExpressionBody) {
+        body = {
             type: 'FunctionBody',
             directives: obj.directives,
             statements: body
         };
     }
+
+    obj.body = body;
 }
 
-function convertObject(key, value) {}
+/**
+ * Uses the same signature as JSON.parse callback, but limited to objects.
+ * @this - The parent object being parsed.
+ * @param {string} key - The current key in that object.
+ * @param {object} obj - The value being converted and added to the object.
+ * @returns {object} Converted node.
+ */
+function convertObject(key, obj) {
+    switch (obj.type) {
+        case 'Block':
+            switch (this.type) {
+                case 'TryCatchStatement':
+                case 'TryFinallyStatement':
+                case 'CatchClause':
+                    // not used as a statement here, do nothing
+                    break;
+                default:
+                    // otherwise wrap
+                    obj = {
+                        type: 'BlockStatement',
+                        block: obj
+                    };
+                    break;
+            }
+            break;
+        case 'VariableDeclaration':
+            if (
+                this.type !== 'Export' &&
+                !(this.type === 'ForStatement' && key === 'init')
+            ) {
+                // used as a statement, wrap
+                obj = {
+                    type: 'VariableDeclarationStatement',
+                    declaration: obj
+                };
+            }
+            break;
+        case 'LabelledStatement':
+            obj.type = 'LabeledStatement';
+            break;
+        case 'LiteralPropertyName':
+            obj.type = 'StaticPropertyName';
+            break;
+        case 'LiteralRegExpExpression': {
+            let flags = obj.flags;
+            obj.global = flags.includes('g');
+            obj.ignoreCase = flags.includes('i');
+            obj.multiLine = flags.includes('m');
+            obj.sticky = flags.includes('y');
+            obj.unicode = flags.includes('u');
+            break;
+        }
+        case 'ForInOfBinding':
+            obj.type = 'VariableDeclaration';
+            obj.declarators = [
+                {
+                    type: 'VariableDeclarator',
+                    init: null,
+                    binding: obj.binding
+                }
+            ];
+            break;
+        default:
+            tryRemoveFunctionContents(obj);
+            break;
+    }
+
+    return obj;
+}
+
+/**
+ * This is a reviver callback for JSON.parse to convert Binary AST JSON to Shift AST.
+ * @this - The parent object being parsed.
+ * @param {string} key - The current key in that object.
+ * @param {*} value - The value being converted and added to the object.
+ * @returns {*} Converted value.
+ */
+module.exports = function convert(key, value) {
+    if (typeof value === 'object' && value !== null && !Array.isArray(value)) {
+        return convertObject.call(this, key, value);
+    } else {
+        return value;
+    }
+};
