843187ef065c38e6137496c530b69add4e01f08f
David Teller <D.O.Teller@gmail.com>
2019-09-04 14:42:56 +0200

    Context 0.1: Reading the strings table

--- crates/binjs_io/src/context/strings.rs
+++ crates/binjs_io/src/context/strings.rs
@@ -1,6 +1,6 @@
-use context::varnum::{ReadVaru32, WriteVaru32};
+use context::varnum::ReadVaru32;
 
-use std::io::{Error, ErrorKind, Read, Write};
+use std::io::{Error, ErrorKind, Read};
 
 /// The magic header for external strings dictionaries.
 const DICTIONARY_MAGIC_HEADER: &'static [u8; 7] = b"astdict";
@@ -12,8 +12,6 @@ const DICTIONARY_MAGIC_HEADER: &'static [u8; 7] = b"astdict";
 pub struct StringTable {
     strings: Vec<String>,
 }
-
-// ---- Reading
 
 impl StringTable {
     /// The strings in the table, in the order in which they appear in the stream.
@@ -95,84 +93,4 @@ impl ExternalStringDictionary {
         let strings = StringTable::read(input)?;
         Ok(ExternalStringDictionary { strings })
     }
-}
-
-#[test]
-fn test_empty_external_string_dictionary() {
-    let bytes = b"astdict\0";
-    let mut input = std::io::Cursor::new(&bytes);
-    let dictionary = ExternalStringDictionary::read(&mut input).unwrap();
-    assert_eq!(dictionary.strings().len(), 0);
-}
-
-// ----- Writing
-
-impl StringTable {
-    pub fn new() -> Self {
-        StringTable {
-            strings: Vec::new(),
-        }
-    }
-    pub fn with_capacity(capacity: usize) -> Self {
-        StringTable {
-            strings: Vec::with_capacity(capacity),
-        }
-    }
-    pub fn from_strings(strings: Vec<String>) -> Self {
-        StringTable { strings }
-    }
-    pub fn add_string(&mut self, string: String) {
-        self.strings.push(string);
-    }
-    pub fn write(&self, mut out: &mut dyn Write) -> Result<(), Error> {
-        // Write the length.
-        out.write_varu32(self.strings.len() as u32)?;
-
-        // Write each string, NUL-terminated.
-        for string in &self.strings {
-            // Write bytes one by one to handle escapes.
-            for byte in string.as_bytes() {
-                match byte {
-                    0 | 1 =>
-                    /* escape byte */
-                    {
-                        out.write_all(&[1, *byte])
-                    }
-                    _ => out.write_all(&[*byte]),
-                }?;
-            }
-            // NUL terminator
-            out.write_all(&[0])?;
-        }
-
-        Ok(())
-    }
-}
-
-#[test]
-fn test_write_and_read_string_tables() {
-    use std::io::Cursor;
-
-    // Generate samples with a few embedded '0's and '1's.
-    let samples: Vec<String> = (0..300)
-        .map(|len| {
-            (0..len)
-                .map(|i| {
-                    let b = i as u8;
-                    let c: char = b.into();
-                    c
-                })
-                .collect()
-        })
-        .collect();
-
-    let reference = StringTable::from_strings(samples);
-
-    // Write table, read it back.
-    let mut buf = Vec::new();
-    reference.write(&mut buf).unwrap();
-
-    let result = StringTable::read(&mut Cursor::new(buf)).unwrap();
-
-    assert_eq!(result.strings(), reference.strings());
 }
