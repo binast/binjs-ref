d2c978f848b13b5abcd82902aecc74331cf3d029
David Teller <D.O.Teller@gmail.com>
2019-09-04 13:41:18 +0200

    Context 0.1: Generating doc from specs

---
+++ crates/binjs_io/src/context/format.rs
@@ -0,0 +1,6 @@
+// Fake module. This is only a placeholder for documentation.
+
+// As of this writing, doc inclusion only works on Nightly compiler.
+#![cfg_attr(feature = "unstable", doc(include = "../../../spec/context.md"))]
+
+#![cfg_attr(not(feature = "unstable"), doc = "To generate the format spec documentation, please use `cargo doc --features unstable`. This requires Rust Nightly.")]
